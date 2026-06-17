90a968610a412222cea57f88a95c11a5084edbe4
rinsan1120 <keiz5963@gmail.com>
2026-06-17 12:51:27 +0900

    a

---
+++ archives-data.js
@@ -0,0 +1,55 @@
+export const archives = [
+  {
+    id: "event-02",
+    date: "2026.05.16",
+    title: "第二夜",
+    youtubeId: "j3ytnWDjNOE",
+    thumbnail: "https://img.youtube.com/vi/j3ytnWDjNOE/maxresdefault.jpg",
+    program: [
+      {
+        title: "双子の星（二）",
+        author: "宮沢賢治",
+        reader: "唯ノイフ"
+      },
+      {
+        title: "星の王子様（前編）",
+        author: "サン=テグジュペリ",
+        reader: "白羽まちる"
+      },
+      {
+        title: "星の音が聞こえる夜に",
+        author: "あねり",
+        reader: "139(イサク)"
+      },
+      {
+        title: "花をつくる妖精のおはなし",
+        author: "むち",
+        reader: "139(イサク)"
+      },
+    ]
+  },
+  {
+    id: "event-01",
+    date: "2026.04.11",
+    title: "第一夜",
+    youtubeId: "lqz88xjOwz0",
+    thumbnail: "https://img.youtube.com/vi/lqz88xjOwz0/maxresdefault.jpg",
+    program: [
+      {
+        title: "わるい王様（伝説）",
+        author: "アンデルセン",
+        reader: "花咲くバッカス"
+      },
+      {
+        title: "双子の星",
+        author: "宮沢賢治",
+        reader: "唯ノイフ"
+      },
+      {
+        title: "花と人間の話",
+        author: "小川未明",
+        reader: "ʚみけɞ"
+      },
+    ]
+  }
+];
---
+++ members-data.js
@@ -0,0 +1,109 @@
+export const casts = [
+  {
+    name: "花咲くバッカス",
+    comment: "＊＊＊＊＊＊＊＊＊＊＊＊",
+    image: "assets/members/bakkasu.png",
+    links: [
+      {
+        label: "VRChat",
+        url: "https://vrchat.com/home/user/usr_22d5c01d-3007-4822-bed4-6b7d841dbbf7"
+      },
+      {
+        label: " X ",
+        url: "https://x.com/hanasakubakkas"
+      }
+    ]
+  },
+  {
+    name: "唯ノイフ",
+    comment: "今日も元気にVRを散策中！どこでもいつでも、みなさんに声を届けられたら嬉しいです！好きな物はゲーム、ボカロ、音楽、マンガ、アニメ、太宰治、宮沢賢治、倉橋ヨエコ様全曲",
+    image: "assets/members/if.png",
+    links: [
+      {
+        label: " X ",
+        url: "https://x.com/Umaretai12"
+      },
+      {
+        label: "YouTube",
+        url: "https://www.youtube.com/@%E5%94%AF%E4%B9%83%E3%82%A4%E3%83%95"
+      }
+    ]
+  },
+  {
+    name: "ʚみけɞ",
+    comment: "静かな声でゆったりと朗読します。眠れない夜や心を落ち着かせたい夜のおともになれたら幸いです。好き➺ねこ、スイーツ、キラキラ、薄暮の空、朝井リョウ、森博嗣、ベイマックス",
+    image: "assets/members/mike.png",
+    links: [
+    ]
+  },
+  {
+    name: "白羽まちる",
+    comment: "＊＊＊＊＊＊＊＊＊＊＊＊",
+    image: "assets/members/machiru.png",
+    links: [
+      {
+        label: "VRChat",
+        url: "https://vrchat.com/home/user/usr_43f462ca-0592-4848-9de5-f24d84056a3f"
+      },
+      {
+        label: " X ",
+        url: "https://x.com/machiru_vrc"
+      }
+    ]
+  },
+    {
+    name: "139（イサク）",
+    comment: "初めまして。普段は小林勲功（こばやしいさく）というプロの声優としてお仕事をしています。　私が朗読において大事にしていることは、　『作品の文章、言葉に秘められた想いや沈んでいる感情を丁寧に拾い上げることです。』　聴きに来てくれた貴方の心に、素敵なものを残せますように。",
+    image: "assets/members/139.png",
+    links: [
+      {
+        label: "VRChat",
+        url: "https://vrchat.com/home/user/usr_e2b8cd6f-d1c9-4b89-a788-01d8ec424e9b"
+      },
+      {
+        label: " X ",
+        url: "https://x.com/139Magna"
+      },
+            {
+        label: " HP ",
+        url: "https://www.kobayashi-isaku.com/"
+      }
+    ]
+  }
+];
+
+export const staffs = [
+  {
+    name: "龍飛",
+    favorite: "動画撮影",
+    comment: "カメラマンを務める小さきいのち。好きなものは桃、森博嗣作品",
+    image: "assets/members/ryuhi.png",
+    links: [
+      {
+        label: "VRChat",
+        url: "https://vrchat.com/home/user/usr_867c325a-cb05-49cc-b983-7cf02188a9d0"
+      },
+      {
+        label: " X ",
+        url: "https://x.com/doragon1739"
+      }
+    ]
+  },
+  {
+    name: "りん（rinsan1120)",
+    favorite: "企画・運営",
+    comment: "",
+    image: "assets/members/rin.png",
+    links: [
+      {
+        label: "VRChat",
+        url: "https://vrchat.com/home/user/usr_73654b2d-85b3-47e3-8a87-c8596b7c03c5"
+      },
+      {
+        label: " X ",
+        url: "https://x.com/rinsan11201"
+      }
+    ]
+  }
+ 
+];
